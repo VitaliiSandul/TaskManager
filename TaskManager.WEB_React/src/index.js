2e7f4115b5211d0b32c2a023ee7cb0742b57a0f6
vitalii_sandul <vitaliisan@gmail.com>
2020-11-19 21:21:44 +0200

    no message

---
+++ TaskManager.WEB_React/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    Login
+  </BrowserRouter>,
+  document.getElementById("root")
+);
